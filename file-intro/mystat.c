7cf041fa9a6a60938810ba93cf6b71ea25f57892
潘丁瑜 <2413110608@qq.com>
2024-03-17 18:58:17 +0800

    init repos

---
+++ file-intro/mystat.c
@@ -0,0 +1,3 @@
+// 1. Stat: Write your own version of the command line program stat, which simply calls
+// the stat() system call on a given file or directory. Print out file size, number of blocks
+// allocated, reference (link) count, file permissions, and file inode. 
